66859a736269d9e363996066f3add6b6ebf61024
guimey <guillaume.meyer@viacesi.fr>
2024-01-21 14:49:58 +0100

    Ajout projet wpf

---
+++ WpfNegosud/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfNegosud
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
