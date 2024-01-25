3a0380db1050885ebf280030d19485f163438828
guimey <guillaume.meyer@viacesi.fr>
2024-01-25 11:46:28 +0100

    client

---
+++ WpfNegosud/Services/HttpClientService.cs
@@ -0,0 +1,28 @@
+﻿using Negosud.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegosudWpf.Services
+{
+    public static class HttpClientService
+    {
+        private const string baseAddress = "https://localhost:7226/";
+        private static HttpClient? client = null;
+
+        private static HttpClient Client
+        {
+            get
+            {
+                if (client == null)
+                    client = new() { BaseAddress = new Uri(baseAddress) };
+                return client;
+            }
+        }
+
+        public static async Task<ClientDto>
+    }
+}
---
+++ negosud/Dto/ClientDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negosud.Dto
+{
+    public class ClientDto
+    {
+        public int Id { get; set; }
+
+        public string NomClient { get; set; }
+
+        public string PrenomClient { get; set; }
+
+        public string AdresseClient { get; set; }
+
+        public string CodePostalClient { get; set; }
+
+        public string PaysClient { get; set; }
+
+        public string TelephoneClient { get; set; }
+
+        public string EmailClient { get; set; }
+    }
+}
