ef93da31319d842d81b4694767e4f5f7cd64dd7a
Florian <belkadiflorian1@gmail.com>
2024-02-21 13:49:55 +0100

    ASP produit + logo

---
+++ NegosudSite/Controllers/ProduitsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Negosud.Class;
+using Negosud.Context;
+
+namespace NegosudSite.Controllers
+{
+    public class ProduitsController : Controller
+    {
+        private readonly NegosudContext _context;
+
+        public ProduitsController(NegosudContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Produits
+        public async Task<IActionResult> Index()
+        {
+            var negosudContext = _context.Produits.Include(p => p.Domaine).Include(p => p.Type);
+            return View(await negosudContext.ToListAsync());
+        }
+
+        // GET: Produits/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produit = await _context.Produits
+                .Include(p => p.Domaine)
+                .Include(p => p.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            return View(produit);
+        }
+
+        // GET: Produits/Create
+        public IActionResult Create()
+        {
+            ViewData["DomaineID"] = new SelectList(_context.Domaines, "Id", "AdresseDomaine");
+            ViewData["TypeID"] = new SelectList(_context.Types, "Id", "NomType");
+            return View();
+        }
+
+        // POST: Produits/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NomProduit,NbProduit,PrixProduit,Quantite,TypeID,DomaineID")] Produit produit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(produit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DomaineID"] = new SelectList(_context.Domaines, "Id", "AdresseDomaine", produit.DomaineID);
+            ViewData["TypeID"] = new SelectList(_context.Types, "Id", "NomType", produit.TypeID);
+            return View(produit);
+        }
+
+        // GET: Produits/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produit = await _context.Produits.FindAsync(id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+            ViewData["DomaineID"] = new SelectList(_context.Domaines, "Id", "AdresseDomaine", produit.DomaineID);
+            ViewData["TypeID"] = new SelectList(_context.Types, "Id", "NomType", produit.TypeID);
+            return View(produit);
+        }
+
+        // POST: Produits/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NomProduit,NbProduit,PrixProduit,Quantite,TypeID,DomaineID")] Produit produit)
+        {
+            if (id != produit.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(produit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProduitExists(produit.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DomaineID"] = new SelectList(_context.Domaines, "Id", "AdresseDomaine", produit.DomaineID);
+            ViewData["TypeID"] = new SelectList(_context.Types, "Id", "NomType", produit.TypeID);
+            return View(produit);
+        }
+
+        // GET: Produits/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produit = await _context.Produits
+                .Include(p => p.Domaine)
+                .Include(p => p.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            return View(produit);
+        }
+
+        // POST: Produits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var produit = await _context.Produits.FindAsync(id);
+            if (produit != null)
+            {
+                _context.Produits.Remove(produit);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProduitExists(int id)
+        {
+            return _context.Produits.Any(e => e.Id == id);
+        }
+    }
+}
