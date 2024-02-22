20a591742f549fea11724b81c429320b87f4e63f
Florian <belkadiflorian1@gmail.com>
2024-02-22 15:39:43 +0100

    Authentification Terminée
    Tout est fonctionnel

--- WpfNegosud/Models/LoginUser.cs
+++ WpfNegosud/Models/LoginUser.cs
@@ -3,4 +3,7 @@ public class LoginUser
 {
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
+
+    public string twoFactorCode { get; } = "string";
+    public string twoFactorRecoveryCode { get; } = "string";
 }
