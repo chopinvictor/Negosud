204ff128367ba15c9998e4c592a20cfb861971a4
guimey <guillaume.meyer@viacesi.fr>
2024-03-13 14:48:10 +0100

    page login

---
+++ NegosudSite/Controllers/Auth.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NegosudSite.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+
+	public class AuthController : Controller
+	{
+		// Action pour afficher la page de connexion
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		// Action pour gérer le formulaire de connexion
+		[HttpPost]
+		public IActionResult Login(string username, string password)
+		{
+			// Vérification des informations d'identification, par exemple
+			if (username == "utilisateur" && password == "motdepasse")
+			{
+				// Redirection vers une autre action dans le même contrôleur
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				// Redirection vers la même action (la page de connexion) avec un message d'erreur
+				TempData["ErrorMessage"] = "Identifiants incorrects";
+				return RedirectToAction("Login");
+			}
+		}
+	}
+}
