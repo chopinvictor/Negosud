e4199bc23270bcee19864add3b51128496a4dac3
Florian <belkadiflorian1@gmail.com>
2024-04-03 15:29:35 +0200

    Avancée Flo

--- NegosudSite/Controllers/PanierController.cs
+++ NegosudSite/Controllers/PanierController.cs
@@ -7,38 +7,38 @@ namespace NegosudSite.Controllers
 	public class PanierController : Controller
 	{
 
-        public IActionResult AjouterAuPanier(string nom, int nombre)
-        {
-            List<Article> panier;
-            if (Request.Cookies.ContainsKey("Panier"))
-            {
-                panier = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Article>>(Request.Cookies["Panier"]);
-            }
-            else
-            {
-                panier = new List<Article>();
-            }
+        //public IActionResult AjouterAuPanier(string nom, int nombre)
+        //{
+        //    List<Article> panier;
+        //    if (Request.Cookies.ContainsKey("Panier"))
+        //    {
+        //        panier = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Article>>(Request.Cookies["Panier"]);
+        //    }
+        //    else
+        //    {
+        //        panier = new List<Article>();
+        //    }
 
-            panier.Add(new Article { Nom = nom, Nombre = nombre });
+        //    panier.Add(new Article { Nom = nom, Nombre = nombre });
 
-            Response.Cookies.Append("Panier", Newtonsoft.Json.JsonConvert.SerializeObject(panier));
+        //    Response.Cookies.Append("Panier", Newtonsoft.Json.JsonConvert.SerializeObject(panier));
 
-            // Redirection vers l'action "Index" avec le panier
-            return RedirectToAction("Index", new { panier = panier });
-        }
+        //    // Redirection vers l'action "Index" avec le panier
+        //    return RedirectToAction("Index", new { panier = panier });
+        //}
 
 
         public IActionResult Index()
 		{
-			List<Article> panier;
-			if (Request.Cookies.ContainsKey("Panier"))
-			{
-				panier = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Article>>(Request.Cookies["Panier"]);
-			}
-			else
-			{
-				panier = new List<Article>();
-			}
+			//List<Article> panier;
+			//if (Request.Cookies.ContainsKey("Panier"))
+			//{
+			//	panier = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Article>>(Request.Cookies["Panier"]);
+			//}
+			//else
+			//{
+			//	panier = new List<Article>();
+			//}
 			return View();
 		}
 
